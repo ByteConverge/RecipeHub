a79e71e930f51861d5787431dc650ba63cdd50fb
Oyama <godstimeoyama082@gmail.com>
2024-07-24 16:29:19 +0800

    ..

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -12,7 +12,7 @@ export default function AboutPage() {
           <h1 className="text-[#996D3E] sm:text-[40px] font-poppins text-[20px] font-medium">
             What we offer
           </h1>
-          <p className="sm:text-[21px] sm:font-[400] font-poppins text-justify ">
+          <p className="sm:text-[21px] sm:font-[400] font-poppins text-justify text-[.8rem]  ">
             At Recidish, we provide a comprehensive collection of authentic
             Nigerian recipes, complete with detailed instructions, ingredient
             lists, and cooking tips. Our goal is to make it easy for you to
@@ -20,19 +20,19 @@ export default function AboutPage() {
             you are in the world.
           </p>
           <ul className="sm:text-[24px] sm:font-[400] list-inside font-poppins pl-2 pr-3 ">
-            <li className="list-disc text-justify">
+            <li className="list-disc text-justify text-[.8rem]">
               Authentic Recipes: Each recipe on our site is carefully curated to
               ensure authenticity and taste. From traditional classics to modern
               twists, we offer a wide range of dishes that showcase the
               diversity of Nigerian cuisine.
             </li>
-            <li className="list-disc text-justify">
+            <li className="list-disc text-justify text-[.8rem]">
               Step-by-Step Guides: Our recipes are designed to be easy to
               follow, with clear step-by-step instructions and helpful tips.
               Whether you are a novice cook or an experienced chef, you will
               find everything you need to create delicious Nigerian meals.
             </li>
-            <li className="list-disc text-justify ">
+            <li className="list-disc text-justify  text-[.8rem]">
               Community Engagement: We believe that food brings people together.
               Our platform allows you to share your own recipes, rate and review
               others, and connect with fellow Nigerian food enthusiasts. Join
